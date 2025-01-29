7db358332dbdebe3c76b922dffd3b9e44090e8ec
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-01-29 18:31:35 +0200

    connect appwrite api to app. create a middleware

--- monum/src/app/page.tsx
+++ monum/src/app/page.tsx
@@ -1,17 +1,28 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter } from "next/navigation";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { useCurrent } from "@/features/auth/api/use-current";
+import { useLogout } from "@/features/auth/api/use-logout";
 
 export default function Home() {
+  const router = useRouter();
+  const { data, isLoading } = useCurrent();
+  const { mutate } = useLogout();
+
+  useEffect(() => {
+    if (!data && !isLoading) {
+      router.push("/sign-in");
+    }
+  }, [data]);
+
   return (
-    <div className="">
-      <Input/>
-      <Button size="lg">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="destructive">Destructive</Button>
-      <Button variant="ghost">Ghost</Button>
-      <Button variant="muted">Muted</Button>
-      <Button variant="outline">Outline</Button>
-      <Button variant="teritary">Teritary</Button>
-    </div>
-  )
-}
+    <div>
+      Only visible to authorized users.
+      <Button onClick={() => mutate()}>
+        Logout
+      </Button>
+    </div>  
+  );
+};
