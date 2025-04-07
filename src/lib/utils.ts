72d3b27e63c643e4ef52305cda4975a139c68781
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-04-07 16:37:37 +0300

    craete utils for cascade deleting documents in DB

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,8 @@
-import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+
+import { clsx, type ClassValue } from "clsx";
+
+import { Databases, Query } from "node-appwrite";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -21,3 +24,34 @@ export function snakeCaseToTitleCase(str: string) {
     .replace(/_/g, " ")
     .replace(/\b\w/g, (char) => char.toUpperCase());
 }
+
+/**
+ * Утиліта для каскадного видалення документів.
+ * @param databases - Інстанс бази даних Appwrite.
+ * @param databaseId - ID бази даних.
+ * @param collectionId - ID колекції, з якої потрібно видалити документи.
+ * @param filters - Масив фільтрів для пошуку документів.
+ * @param childCollections - Масив дочірніх колекцій для каскадного видалення.
+ */
+export const cascadeDelete = async (
+  databases: Databases,
+  databaseId: string,
+  collectionId: string,
+  filters: string[],
+  childCollections: {
+    collectionId: string;
+    foreignKey: string;
+  }[] = []
+) => {
+  const documents = await databases.listDocuments(databaseId, collectionId, filters);
+
+  for (const document of documents.documents) {
+    for (const child of childCollections) {
+      await cascadeDelete(databases, databaseId, child.collectionId, [
+        Query.equal(child.foreignKey, document.$id),
+      ]);
+    }
+
+    await databases.deleteDocument(databaseId, collectionId, document.$id);
+  }
+};
