03b89b6ceb5f6859b0576663f70fec0d4539029e
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-05-02 13:51:37 +0300

    change color text of button

--- src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/client.tsx
@@ -108,7 +108,7 @@ export const TasksList = ({ data, total }: TasksListProps) => {
               No task found
             </li>
           </ul>
-          <Button variant="muted" className="mt-4 w-full dark:hover:text-neutral-200" asChild>
+          <Button variant="muted" className="mt-4 w-full dark:hover:text-neutral-500" asChild>
             <Link href={`/workspaces/${workspaceId}/tasks`}>
               Show all
             </Link>
