1cf05218122cebed1491233e856f499878cda99a
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-05-07 10:40:36 +0300

    fix deploy bugs

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -3,14 +3,18 @@ import { redirect } from "next/navigation";
 import { getCurrent } from "@/features/auth/queries";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
 
-const SignUpPage = async ({ params }: { params: { redirect: string }; }) => {
+interface SignUpPageProps {
+    searchParams: {
+      redirect?: string;
+    };
+}
+
+const SignUpPage = async ({ searchParams }: SignUpPageProps) => {
     const user = await getCurrent();
 
-    if (user) {
-        if (params.redirect) redirect(params.redirect as string);
+    const redirectString = searchParams.redirect ?? '/';
 
-        redirect("/");
-    }
+    if (user) redirect(redirectString);
 
     return <SignUpCard />
 };
