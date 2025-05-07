a914aca460b39a8068754db769d9ad7e3935d2a6
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-05-07 10:25:41 +0300

    add redirect when went from gmail link

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,11 +1,17 @@
-import { redirect } from "next/navigation";
+import { redirect, useParams } from "next/navigation";
+
 import { getCurrent } from "@/features/auth/queries";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
 
 const SignUpPage = async () => {
     const user = await getCurrent();
+    const params = useParams();
 
-    if (user) redirect("/");
+    if (user) {
+        if (params.redirect) redirect(params.redirect as string);
+        
+        redirect("/");
+    }
 
     return <SignUpCard />
 };
--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/page.tsx
@@ -4,9 +4,17 @@ import { TaskIdClient } from "./client";
 
 import { getCurrent } from "@/features/auth/queries"
 
-const TaskIdPage = async () => {
+interface TaskIdPageProps {
+  params: {
+    workspaceId: string;
+    taskId: string;
+  };
+};
+
+const TaskIdPage = async ({ params }: TaskIdPageProps) => {
   const user = await getCurrent();
-  if (!user) redirect("/sign-up");
+
+  if (!user) redirect(`/sign-up?redirect=/dashboard/workspaces/${params.workspaceId}/tasks/${params.taskId}`);
 
   return <TaskIdClient />
 }
