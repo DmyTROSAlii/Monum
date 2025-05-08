0e8695e7d5cbeb4b1e8bfa9957dd8719d57b1208
DmytrODemoN <accwebdmytro38140@gmail.com>
2025-05-08 12:34:00 +0300

    relocate redirect searchParam from query to cookies

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,22 +1,18 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
-
-// Removed invalid import for PageProps
 
 import { getCurrent } from "@/features/auth/queries";
 import { SignUpCard } from "@/features/auth/components/sign-up-card";
-import { PageProps } from "../../../../.next/types/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/page";
 
-export default async function SignUpPage({
-    searchParams,
-  }: {
-    searchParams?: PageProps["searchParams"];
-  }) {
-    const user = await getCurrent();
+export default async function SignUpPage() {
+  const user = await getCurrent();
 
-    const resolvedSearchParams = await searchParams;
-    const redirectUrl = resolvedSearchParams?.redirect || "/";
+  if (user) {
+    const cookieStore = cookies();
+    const redirectPath = (await cookieStore).get("redirectAfterAuth")?.value || "/";
 
-    if (user) redirect(redirectUrl);
+    redirect(redirectPath);
+  }
 
-    return <SignUpCard />;
+  return <SignUpCard />;
 }
--- src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/tasks/[taskId]/page.tsx
@@ -1,20 +1,24 @@
+import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 import { TaskIdClient } from "./client";
 
 import { getCurrent } from "@/features/auth/queries"
 
-interface TaskIdPageProps {
-  params: {
-    workspaceId: string;
-    taskId: string;
-  };
-};
-
-const TaskIdPage = async ({ params }: TaskIdPageProps) => {
+const TaskIdPage = async ({ params }: { params: Promise<{workspaceId: string; taskId: string; }> }) => {
   const user = await getCurrent();
 
-  if (!user) redirect(`/sign-up?redirect=/workspaces/${params.workspaceId}/tasks/${params.taskId}`);
+  if (!user) {
+    const { workspaceId, taskId } = await params;
+
+    (await cookies()).set("redirectAfterAuth", `/workspaces/${workspaceId}/tasks/${taskId}`, {
+      httpOnly: true,
+      path: "/",
+      secure: process.env.NODE_ENV === "production",
+    });
+
+    redirect("/sign-up");
+  }
 
   return <TaskIdClient />
 }
